c67d2b251190b0eab93019ba2447e66206ae775a
jekxkan <smirnova.eyu@mail.ru>
2025-06-27 01:24:04 +0300

    without filtering direction

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ from img import IMGWorker
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 video = cv2.VideoCapture(1)
+video.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+video.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
-# ser = serial.Serial('', 9600, timeout=1)
+ser = serial.Serial('', 9600, timeout=1)
 
 command = {
     'No line': 'STOP',
@@ -26,7 +28,6 @@ while True:
 
         img_worker = IMGWorker(frame)
         output_img = img_worker.draw_white_objects()
-        highlighted_img = img_worker.increase_img_brightness()
         trajectory = img_worker.draw_trajectory()
 
         cv2.imshow('Finded contours', output_img)
@@ -34,31 +35,29 @@ while True:
 
         direction = img_worker.analyze_trajectory()
 
-        if last_direction is None:
-            last_direction = direction
-            counter = 1
-            logging.info(f'Текущее направление: {last_direction}')
-        elif direction == last_direction:
-            counter = 0
-        else:
-            counter += 1
-            if counter >= 5:
-                last_direction = direction
-                counter = 0
-                logging.info(f'Направление изменилось на: {last_direction}')
+        # if last_direction is None:
+        #     last_direction = direction
+        #     counter = 1
+        #     logging.info(f'Текущее направление: {last_direction}')
+        # elif direction == last_direction:
+        #     counter = 0
+        # else:
+        #     counter += 1
+        #     if counter >= 5:
+        #         last_direction = direction
+        #         counter = 0
+        logging.info(f'Направление изменилось на: {direction}')
 
-                # ser.write((command[last_direction] + '/n').encode())
-                # logging.info(f'Отправили команду {direction}')
-                # response = ser.readline().decode().strip()
-                # logging.info(f'Ответ ардуино: {response}')
+        ser.write((command[direction]).encode())
+        logging.info(f'Отправили команду {direction}')
+        response = ser.readline().decode().strip()
+        logging.info(f'Ответ ардуино: {response}')
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     except Exception as e:
         logging.error(f'Ошибка: {e}')
-    # finally:
-    #     ser.close()
-
+ser.close()
 video.release()
 cv2.destroyAllWindows()
